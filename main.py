088d76f21c1ba391717fb14a989401d13e387e25
Blaine Silva <blainesilva16@gmail.com>
2025-05-22 20:06:06 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, jsonify, request,send_file
+from flask_bootstrap import Bootstrap5
+from PIL import Image
+import numpy as np
+from collections import Counter
+from colorthief import ColorThief
+import dotenv,os
+
+dotenv.load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+Bootstrap5(app)
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    return render_template('index.html')
+
+
+def get_top_colors_by_region(image: Image.Image, num_sections: int = 10):
+    image = image.convert("RGB")
+    width, height = image.size
+    section_height = height // num_sections
+    result_colors = []
+
+    for i in range(num_sections):
+        left = i * section_height
+        right = (i + 1) * section_height if i < num_sections - 1 else height
+        cropped = image.crop((0, left, width, right))
+
+        pixels = np.array(cropped).reshape(-1, 3)
+        pixels = [tuple(map(int, pixel)) for pixel in pixels]  # Ensure RGB values are ints
+
+        most_common_color, _ = Counter(pixels).most_common(1)[0]
+
+        r, g, b = most_common_color
+
+        color_info = {
+            'rgb': f'rgb({r}, {g}, {b})',
+            'hex': '#{:02x}{:02x}{:02x}'.format(r, g, b)
+        }
+
+        result_colors.append(color_info)
+
+    return result_colors
+
+@app.route("/get-dominant-colors", methods=["POST"])
+def get_dominant_colors():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    image_file = request.files['image']
+    # sections = request.form.get('sections', default=10, type=int)
+
+    try:
+        # image = Image.open(image_file)
+        color_thief = ColorThief(image_file)
+        # colors = get_top_colors_by_region(image, num_sections=sections)
+        colors_rgb = color_thief.get_palette(color_count=11,quality=5)
+        colors = []
+        for color_rgb in colors_rgb:
+            r,g,b = color_rgb
+            color_info = {
+                'rgb': f'rgb({r}, {g}, {b})',
+                'hex': '#{:02x}{:02x}{:02x}'.format(r, g, b)
+            }
+            colors.append(color_info)
+        print(colors)
+        return jsonify({"colors": colors})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
